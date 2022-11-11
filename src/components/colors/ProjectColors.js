0a7e42d3848fb76d5463b962ed8d58ffecb73234
prakash <prakash.ranjan1@getfareye.com>
2022-11-11 18:17:24 +0530

    login and sign up Screen ui

---
+++ src/components/colors/ProjectColors.js
@@ -0,0 +1,9 @@
+export const ProjectColors = {
+  white: '#FFFFFF',
+  mint: '#CFF5E7',
+  mintDarker: '#A0E4CB',
+  teal: '#59C1BD',
+  navy: '#0D4C92',
+  black: '#000000',
+  grey: '#CCCCCC',
+};
