b82cabb90ab88132f15077a14b241576b9f01293
prakash <prakash.ranjan1@getfareye.com>
2022-11-16 23:24:11 +0530

    category

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -24,7 +24,7 @@ import {
 initializeApp(firebaseConfig);
 
 const ImageUpload = () => {
-  const [url, setUrl] = React.useState('');
+  //const [url, setUrl] = React.useState({});
 
   let options = {
     title: 'Select Image',
@@ -48,29 +48,66 @@ const ImageUpload = () => {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       }
-      console.log(response);
-      setUrl(response.url);
-      if (response != null) {
-        const storage = getStorage(); //the storage itself
-        const ref1 = ref(storage, 'image.jpg'); //how the image will be addressed inside the storage
+      // console.log(response);
+      // setUrl(response);
+      // if (response != null) {
+      //   const storage = getStorage(); //the storage itself
+      //   const ref1 = ref(storage, 'image.jpg'); //how the image will be addressed inside the storage
 
-        //convert image to array of bytes
-        const img = fetch(response.uri);
-        const bytes = img.blob();
+      //   //convert image to array of bytes
+      //   const img = fetch(response.uri);
+      //   const bytes = img.blob();
 
-        uploadBytes(ref1, bytes); //upload images
-      }
+      //   uploadBytes(ref1, bytes); //upload images
+      // }
     });
   };
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={
+          {
+            //uri: 'data:image/jpeg;base64,' + url.uri,
+          }
+        }
+      />
       <TouchableOpacity onPress={pickImage}>
-        <Text>Pick Image and Upload File</Text>
+        <Text style={styles.button}>Pick Image From Gallery</Text>
       </TouchableOpacity>
-      <Text>{url}</Text>
+      <TouchableOpacity onPress={pickImage}>
+        <Text style={styles.button}>Upload To Firebase</Text>
+      </TouchableOpacity>
+      <Text>{}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginBottom: 20,
+  },
+  button: {
+    fontSize: 15,
+    padding: 10,
+    width: 200,
+    color: 'white',
+    fontWeight: 'bold',
+    marginBottom: 30,
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2874F0',
+    borderRadius: 10,
+    textAlign: 'center',
+  },
+});
 
 export default ImageUpload;
